57f40804b91cc6b42a843222b8fff26da6437636
arjitkaurarora8 <arjitkaurarora8@gmail.com>
2019-11-20 14:32:29 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import LoginComponent from './Login/login';
+import SignupComponent from './Signup/signup';
+import DashboardComponent from './Dashboard/dashboard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require("firebase");
+require("firebase/firestore"); // Required for side-effects?????
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAX_UWuqDXk_X0xoDZ_UtcbgC3_gMVv4Z4",
+    authDomain: "justalk-7820b.firebaseapp.com",
+    databaseURL: "https://justalk-7820b.firebaseio.com",
+    projectId: "justalk-7820b",
+    storageBucket: "justalk-7820b.appspot.com",
+    messagingSenderId: "255288142454",
+    appId: "1:255288142454:web:e3f077f359f9ba58b00d57",
+    measurementId: "G-72ZK9H498B"
+});
+
+const routing = (
+  <Router>
+    <div id='routing-container'>
+      <Route path='/login' component={LoginComponent}></Route>
+      <Route path='/signup' component={SignupComponent}></Route>
+      <Route path='/dashboard' component={DashboardComponent}></Route>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
